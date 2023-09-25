1c8ef30fe89a630c15872e6a1912609dc5c9418d
“AngieNC” <angienathalia.32276@gmail.com>
2023-09-25 13:21:27 -0500

    docs: :memo: Api segunda
    Se añaden elementos prueba para enviar el valor de un rango a una base, para luego emplearla en las tarjetas.

--- main.js
+++ main.js
@@ -1,3 +1,46 @@
+const url = "https://6509e7ebf6553137159c3aee.mockapi.io/Pokemon";
+let prueba = document.querySelector('form')
+
+
+prueba.addEventListener("submit" ,async(e)=>{
+    e.preventDefault();
+
+    let enviar = Object.fromEntries(new FormData (e.target));
+
+    const res = await (await fetch(url)).json();
+
+    
+
+    let config ={
+        method: "POST",
+        headers: {"content-type": "application/json"},
+        body: JSON.stringify(enviar)
+    }
+
+    const x = await (await fetch(url,config)).json();
+
+    console.log(x)
+})
+
+document.addEventListener("DOMContentLoaded", async(e)=>{
+
+    const tabla = document.querySelector("#data-tabla");
+
+    let res = await (await fetch(url)).json();
+
+    res.map((elemento)=>{
+        tabla.insertAdjacentHTML("beforeend", `
+        <tr>
+            <td>${elemento.id}</td>
+            <td>${elemento.hp}</td>
+            <td>${elemento.attack}</td>
+        </tr>
+        `);
+    });
+
+})
+
+
 async function getpokemon() {
     try {
         const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=200");
@@ -31,19 +74,34 @@ async function pokemon() {
             showImageButton.addEventListener("click", () => {
                 const imageUrl = pokemonData.sprites.front_default;
 
-               
+               //ESTO SE UTILIZA
                 Swal.fire({
+
                     title: `${pokemonData.name}`,
                     imageUrl: imageUrl,
                     imageAlt: "Imagen del Pokémon",
-                    html: `${pokemonData.stats.map((data) => `
-                    <input type="range" value="${data.base_stat}">
-                    <label> ${data.stat.name}</label><br>
-                    `).join("")}`,
+                    html: `
+                    <form>
+                        ${pokemonData.stats.map((data) =>`
+                        <div>
+                            <input type="range" value="${data.base_stat}" name="${data.stat.name}">
+                            <label data-name="${data.stat.name}"><b>${data.base_stat}<b> ${data.stat.name}</label><br>
+                        </div>
+                        `).join("")}
+                            <input type="submit" value="Enviar"/> 
+                    </form>
+                            
+                        `,//Esto
                     imageWidth: "80%",
                     imageHeight: "80%",
-                    
+
                     showCloseButton: true,
+                });
+                
+                let myContainer = document.querySelector('#swal2-html-container');
+                myContainer.addEventListener("input", (e)=>{
+                    let myLabel = e.target.nextElementSibling;
+                    myLabel.innerHTML = `<b>${e.target.value}</b> ${myLabel.dataset.name}` 
                 });
             });
         } catch (error) {
@@ -52,5 +110,5 @@ async function pokemon() {
     }
 }
 
-
 pokemon();
+
