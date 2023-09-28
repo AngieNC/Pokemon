b0f76b6c6f7b6f0bf32ccbe08e77983525c4834a
“AngieNC” <139162495+AngieNC@users.noreply.github.com>
2023-09-27 22:04:57 -0500

    docs: :memo: Actualizacion de codigo
    Se le agrega el metodo Post y se cambia un poco el codigo

--- main.js
+++ main.js
@@ -2,113 +2,81 @@ const url = "https://6509e7ebf6553137159c3aee.mockapi.io/Pokemon";
 let prueba = document.querySelector('form')
 
 
-prueba.addEventListener("submit" ,async(e)=>{
-    e.preventDefault();
+const vengaPokemon = async () =>{
 
-    let enviar = Object.fromEntries(new FormData (e.target));
-
-    const res = await (await fetch(url)).json();
-
-    
-
-    let config ={
-        method: "POST",
-        headers: {"content-type": "application/json"},
-        body: JSON.stringify(enviar)
-    }
-
-    const x = await (await fetch(url,config)).json();
-
-    console.log(x)
-})
-
-document.addEventListener("DOMContentLoaded", async(e)=>{
-
-    const tabla = document.querySelector("#data-tabla");
-
-    let res = await (await fetch(url)).json();
-
-    res.map((elemento)=>{
-        tabla.insertAdjacentHTML("beforeend", `
-        <tr>
-            <td>${elemento.id}</td>
-            <td>${elemento.hp}</td>
-            <td>${elemento.attack}</td>
-        </tr>
-        `);
-    });
-
-})
-
-
-
-async function vengaPokemon() {
-    try {
-        const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=200");
-        const data = await res.json();
-        return data.results.map(pokemon => pokemon.name);
-
-
-    } catch (error) {
-        console.error("Error en la lista de nombres de Pokémon:", error);
-        return [];
-    }
+    const res = await(await fetch("https://pokeapi.co/api/v2/pokemon?limit=200")).json();
+    return res.results.map(pokemon => pokemon.name)
 }
 
 
-async function pokemon() {
+const pokemon = async()=> {
     const nombres = await vengaPokemon();
     const principal = document.querySelector("section");
     for (const name of nombres) {
-        try {
-            const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-            const pokemonData = await res.json();
-            const article = document.createElement("article");
-            article.innerHTML = `
-            <img src="${pokemonData.sprites.front_default}"/>  
-            <button class="botones" src="${pokemonData.sprites.front_default}"><h2 id="h2">${name}</h2></button>
-            <img src="${pokemonData.sprites.front_default}"/>
-            `;
-            principal.appendChild(article);
+        
+        const res = await (await  fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)).json();
+        const article = document.createElement("article");
+        article.innerHTML = `
+        <img src="${res.sprites.front_default}"/>  
+        <button class="botones" src="${res.sprites.front_default}"><h2 id="h2">${name}</h2></button>
+        <img src="${res.sprites.front_default}"/>
+        `;
+        principal.appendChild(article);
 
 
-            const muestreBoton = article.querySelector(".botones");
-            muestreBoton.addEventListener("click", () => {
-                const imageUrl = pokemonData.sprites.front_default;
+        const muestreBoton = article.querySelector(".botones");
+        muestreBoton.addEventListener("click", () => {
+            const imageUrl = res.sprites.front_default;
 
-               //ESTO SE UTILIZA
-                Swal.fire({
+            //ESTO SE UTILIZA
+            Swal.fire({
 
-                    title: `${pokemonData.name}`,
-                    imageUrl: imageUrl,
-                    imageAlt: "Imagen del Pokémon",
-                    html: `
-                    <form>
-                        ${pokemonData.stats.map((data) =>`
-                        <div>
-                            <input type="range" value="${data.base_stat}" name="${data.stat.name}">
-                            <label data-name="${data.stat.name}"><b>${data.base_stat}<b> ${data.stat.name}</label><br>
-                        </div>
-                        `).join("")}
-                            <input type="submit" value="Enviar"/> 
-                    </form>
-                            
-                        `,//Esto
-                    imageWidth: "80%",
-                    imageHeight: "80%",
-
-                    showCloseButton: true,
-                });
-                
-                let myContainer = document.querySelector('#swal2-html-container');
-                myContainer.addEventListener("input", (e)=>{
-                    let myLabel = e.target.nextElementSibling;
-                    myLabel.innerHTML = `<b>${e.target.value}</b> ${myLabel.dataset.name}` 
-                });
+                title: `${res.name}`,
+                imageUrl: imageUrl,
+                imageAlt: "Imagen del Pokémon",
+                html: `
+                <form>
+                    ${res.stats.map((data) =>`
+                    <div>
+                        <input type="range" value="${data.base_stat}" name="${data.stat.name}">
+                        <label data-name="${data.stat.name}"><b>${data.base_stat}<b> ${data.stat.name}</label><br>
+                    </div>
+                    `).join("")}
+                        <input type="submit" value="Enviar"/> 
+                </form>
+                        
+                    `,//Esto
+                imageWidth: "80%",
+                imageHeight: "80%"
             });
-        } catch (error) {
-            console.error(`Error de información del ${name}:`, error);
-        }
+
+    muestreBoton.addEventListener("submit" ,async(e)=>{
+          e.preventDefault();
+    
+        let enviar = Object.fromEntries(new FormData (e.target));
+    
+        const res = await (await fetch(url)).json();
+    
+                
+            
+                let config ={
+                    method: "POST",
+                    headers: {"content-type": "application/json"},
+                    body: JSON.stringify(enviar)
+                }
+            
+                const x = await (await fetch(url,config)).json();
+            
+                console.log(x)
+            })
+
+            let contenedor = document.querySelector('#swal2-html-container');
+            contenedor.addEventListener("input", (e)=>{
+                let hermano = e.target.nextElementSibling;
+                hermano.innerHTML = `<b>${e.target.value}</b> ${hermano.dataset.name}` 
+            });
+        });
+        
     }
 }
 
